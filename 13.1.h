2693ab1be1ec43420157310a67cc52d22ddb2917
pelagius2009 <pelagius2009@gmail.com>
2024-12-04 16:59:47 +0800

    20241204-1

---
+++ 13.1.h
@@ -0,0 +1,62 @@
+//13.1.h
+#ifndef CD_H_
+#define CD_H_
+
+//base class
+class Cd{
+	private:
+char performers[50];
+char label[20];
+int selections;
+double playtime;
+
+public:
+Cd(char * s1, char * s2, int n, double x);
+Cd(const Cd & d);
+Cd();
+~Cd();
+virtual void Report() const;
+virtual Cd & operator = (const Cd & d);
+};
+
+//
+class Classic:public Cd
+{
+private:
+char ms_ps[50];
+
+public:
+virtual void Report() const;
+Classic(char *s1, char *s2, char *s3, int n, double x);
+Classic(const Classic & d);
+Classic();
+~Classic();
+
+	 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
